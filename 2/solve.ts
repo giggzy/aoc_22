822e3dcfdddb69ded86c306baab0d58192311203
Gabriel Farrell <farrellg@mskcc.org>
2022-12-19 18:31:01 -0500

    Day 2 working solution for part 2

--- 2/solve.ts
+++ 2/solve.ts
@@ -27,6 +27,14 @@ class Move {
   public isDraw(move: Move): boolean {
     return this.toString() === move.toString();
   }
+
+  public get_winning_move(): Move {
+    throw new Error("not implemented");
+  }
+
+  public get_losing_move(): Move {
+    throw new Error("not implemented");
+  }
 }
 
 class Rock extends Move {
@@ -36,6 +44,13 @@ class Rock extends Move {
   }
   public isWin(move: Move): boolean {
     return move instanceof Sissors;
+  }
+
+  public get_winning_move(): Move {
+    return new Paper();
+  }
+  public get_losing_move(): Move {
+    return new Sissors();
   }
 }
 
@@ -47,6 +62,12 @@ class Paper extends Move {
   public isWin(move: Move): boolean {
     return move instanceof Rock;
   }
+  public get_winning_move(): Move {
+    return new Sissors();
+  }
+  public get_losing_move(): Move {
+    return new Rock();
+  }
 }
 class Sissors extends Move {
   score: number = 3;
@@ -55,6 +76,12 @@ class Sissors extends Move {
   }
   public isWin(move: Move): boolean {
     return move instanceof Paper;
+  }
+  public get_winning_move(): Move {
+    return new Rock();
+  }
+  public get_losing_move(): Move {
+    return new Paper();
   }
 }
 
@@ -73,7 +100,7 @@ function playRPS(a: Move, b: Move): number {
   return 0;
 }
 
-function processFile() {
+function playPartOne() {
   let total_score = 0;
   for (let line of input.split("\n")) {
     // do something with each line
@@ -98,18 +125,42 @@ function processFile() {
   return total_score;
 }
 
+function playPartTwo() {
+  let total_score = 0;
+  for (let line of input.split("\n")) {
+    // do something with each line
+
+    // parse line
+    let v = line.split(" ");
+    let opp_play = v[0];
+    let my_play = v[1];
+
+    // decode to move
+    let opp_move: Move = decode_opp_move(opp_play);
+    let my_move: Move = decode_my_move_part_two(my_play, opp_move);
+
+    const score = playRPS(opp_move, my_move);
+    total_score += score;
+  }
+  return total_score;
+}
+
 // print out total score
+console.log("----------- PART ONE -------------");
+console.log("Grand Total: -> " + playPartOne());
 console.log("------------------------");
-console.log("Grand Total: -> " + processFile());
-console.log("Should be: 11475");
-console.log("------------------------");
-console.log("done");
+console.log("Part one done");
 
 console.log("---------- Debugging example input -------------");
 console.log(playRPS(new Rock(), new Paper()));
 console.log(playRPS(new Paper(), new Rock()));
 console.log(playRPS(new Sissors(), new Sissors()));
 console.log("------------------------");
+
+console.log("----------- PART TWO -------------");
+console.log("Grand Total: -> " + playPartTwo());
+console.log("------------------------");
+console.log("Part two done");
 
 function decode_my_move(my_play: string) {
   let my_move: Move = new Rock();
@@ -122,6 +173,28 @@ function decode_my_move(my_play: string) {
       break;
     case "Z":
       my_move = new Sissors();
+      break;
+    default:
+      console.warn("my_play error, should not get here", my_play);
+      break;
+  }
+  return my_move;
+}
+
+function decode_my_move_part_two(my_play: string, opp_move: Move) {
+  let my_move: Move = new Rock();
+  switch (my_play) {
+    case "X":
+      // need to lose
+      my_move = opp_move.get_losing_move();
+      break;
+    case "Y":
+      // need tp draw, i.e. same move as opp
+      my_move = opp_move;
+      break;
+    case "Z":
+      // need to win
+      my_move = opp_move.get_winning_move();
       break;
     default:
       console.warn("my_play error, should not get here", my_play);
